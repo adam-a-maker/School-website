350f4d9eb8747243aa4cf1e3e8b03bb7338ebf54
adam-a-maker <dam.abd000@gmail.com>
2026-05-12 18:26:49 +0200

    I made changes

--- script.js
+++
@@ -1,6 +0,0 @@
-//https://developer.mozilla.org/en-US/docs/Web/API/Element/scroll_event
-
-const modelViewer = document.querySelector('#Rafale1');
-document.body.onscroll = (event) => {
-    modelViewer.cameraOrbit = "6deg " + -(window.scrollY / 10) + "deg 2.5m";
-};
